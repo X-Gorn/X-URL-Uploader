73d1060a467c7c61c4cd1cad6ecf08f7276aa006
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2020-10-16 15:09:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(683538773)
+    Config.AUTH_USERS.add(958850850)
     app.run()
