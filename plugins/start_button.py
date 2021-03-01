85f3418ef976cb73b08708741b3b6094a11e0565
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-03-01 23:16:30 +0900

    Update start_button.py

--- plugins/start_button.py
+++ plugins/start_button.py
@@ -1,14 +1,41 @@
-import os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
 import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import asyncio
+import json
+import math
+import os
+import shutil
+import time
+from datetime import datetime
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
 from pyrogram.client import Client 
 from pyrogram.client.filters import filters
 from pyrogram.client.types import InlineKeyboardButton, InlineKeyboardMarkup
 from translation import Translation
 
-logger = logging.getLogger(__name__)
 
-
-@Client.on_message(filters.command('start'))
+@pyrogram.Client.on_message(filters.command('start'))
 async def start(bot, message):
     """Start command handler"""
     buttons = [[
