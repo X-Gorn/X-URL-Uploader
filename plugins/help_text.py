30a06e2c32704ae73ee63d185092a7222c95c130
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-03-01 23:41:03 +0900

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -23,6 +23,8 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+from pyrogram.client.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from helper_funcs.chat_base import TRChatBase
 
 def GetExpiryDate(chat_id):
