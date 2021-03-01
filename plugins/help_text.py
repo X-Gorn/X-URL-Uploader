2efadea1c93cf25973cbbec140736a39cd3b206f
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-03-01 23:32:50 +0900

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -66,6 +66,10 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
+        buttons = [[
+        InlineKeyboardButton('Channel', url='https://t.me/xTeamBots'),
+        InlineKeyboardButton('Master', url='https://t.me/xgorn'),
+    ]],
         reply_to_message_id=update.message_id
     )
 
