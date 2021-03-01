ba35248c56ca5a575df4675d6e20bda341ca9cdb
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-03-01 21:15:39 +0900

    Create start_button.py

---
+++ plugins/start_button.py
@@ -0,0 +1,20 @@
+import os
+import logging
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from translation import START_TEXT
+
+logger = logging.getLogger(__name__)
+
+
+@pyrogram.Client.on_message(filters.command('start'))
+async def start(bot, message):
+    """Start command handler"""
+    buttons = [[
+        InlineKeyboardButton('Channel', url='https://t.me/xTeamBots'),
+        InlineKeyboardButton('Master', url='https://t.me/xgorn'),
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply(START_TEXT, reply_markup=reply_markup)
+
+
