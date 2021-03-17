bcc4bb1efeef9fc56d61e44bd72dfe2dd56d7b92
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-03-18 08:22:45 +0900

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -23,7 +23,7 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from pyrogram.client.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
 
 from helper_funcs.chat_base import TRChatBase
 
