3ac4dbed27db7e4c72e2d564ff63994da4752c23
X-Noid <xgorntv@gmail.com>
2021-07-05 05:07:18 +0900

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -65,9 +65,9 @@ async def echo(bot, update):
     youtube_dl_password = None
     file_name = None
     folder = f'./{update.from_user.id}/zippyshare/'
-    bypass = 'zippyshare' or 'files' or 'racaty' or 'hxfile' or 'letsupload' or 'mediafire' or 'sbembed' or 'streamsb' or 'streamtape' or 'uservideo' or 'anonfiles'
+    bypass = ['zippyshare', 'files', 'racaty', 'hxfile', 'letsupload', 'mediafire', 'sbembed', 'streamsb', 'streamtape', 'uservideo', 'anonfiles']
     ext = tldextract.extract(url)
-    if bypass == ext.domain:
+    if ext.domain in bypass:
         pablo = await update.reply_text('LK21 link detected')
         time.sleep(2.5)
         if os.path.isdir(folder):
