8755df1034a1c6489d793b96df47ac7aa9b34ad3
X-Noid <xgorntv@gmail.com>
2021-07-05 03:49:15 +0900

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -29,7 +29,7 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from helper_funcs.display_progress import humanbytes, lk21_run
+from helper_funcs.display_progress import humanbytes
 from helper_funcs.help_uploadbot import DownLoadFile
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
