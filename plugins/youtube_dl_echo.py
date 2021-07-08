9d85d7ecbcb9a31596093a76132880d64b34628e
X-Noid <xgorntv@gmail.com>
2021-07-08 19:02:37 +0900

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -74,7 +74,7 @@ async def echo(bot, update):
             await pablo.edit_text('Uploading...')
             start_time = time.time()
             if xfiletype in ['video/mp4', 'video/x-matroska', 'video/webm']:
-                video = await bot.send_video(
+                await bot.send_video(
                     chat_id=update.chat.id,
                     video=dldir,
                     caption=file_name,
@@ -87,10 +87,8 @@ async def echo(bot, update):
                         start_time
                     )
                 )
-                video_f = await video.forward(Config.LOG_CHANNEL)
-                await video_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>" + '\nLK21 URL: ' + url)
             elif xfiletype == 'audio/mpeg':
-                audio = await bot.send_audio(
+                await bot.send_audio(
                     chat_id=update.chat.id,
                     audio=dldir,
                     caption=file_name,
@@ -103,10 +101,8 @@ async def echo(bot, update):
                         start_time
                     )
                 )
-                audio_f = await audio.forward(Config.LOG_CHANNEL)
-                await audio_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>" + '\nLK21 URL: ' + url)
             else:
-                doc = await bot.send_document(
+                await bot.send_document(
                     chat_id=update.chat.id,
                     document=dldir,
                     caption=file_name,
@@ -118,8 +114,6 @@ async def echo(bot, update):
                         start_time
                     )
                 )
-                doc_f = await doc.forward(Config.LOG_CHANNEL)
-                await doc_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>" + '\nLK21 URL: ' + url)
             await pablo.delete()
             shutil.rmtree(folder)
             return
