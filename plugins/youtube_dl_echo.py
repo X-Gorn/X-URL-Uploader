2b7bf0dfedd72a3bf39084580f4bff9f076dca03
X-Noid <xgorntv@gmail.com>
2021-07-08 18:45:37 +0900

    .

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) Shrimadhav U K | X-Noid
 
 # the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-import asyncio
-import json
-import math
-import os
-import time
+import lk21, urllib.parse, filetype, shutil, time, tldextract, asyncio, json, math, os
 from PIL import Image
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -30,7 +25,8 @@ from helper_funcs.display_progress import humanbytes
 from helper_funcs.help_uploadbot import DownLoadFile
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+from pyrogram.errors import UserNotParticipant
+
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
@@ -40,6 +36,93 @@ async def echo(bot, update):
         youtube_dl_username = None
         youtube_dl_password = None
         file_name = None
+        folder = f'./lk21/{update.from_user.id}/'
+        bypass = ['zippyshare', 'hxfile', 'mediafire', 'anonfiles']
+        ext = tldextract.extract(url)
+        if ext.domain in bypass:
+            pablo = await update.reply_text('LK21 link detected')
+            time.sleep(2.5)
+            if os.path.isdir(folder):
+                await update.reply_text("Don't spam, wait till your previous task done.")
+                await pablo.delete()
+                return
+            os.makedirs(folder)
+            await pablo.edit_text('Downloading...')
+            bypasser = lk21.Bypass()
+            xurl = bypasser.bypass_url(url)
+            if ' | ' in url:
+                url_parts = url.split(' | ')
+                url = url_parts[0]
+                file_name = url_parts[1]
+            else:
+                if xurl.find('/'):
+                    urlname = xurl.rsplit('/', 1)[1]
+                file_name = urllib.parse.unquote(urlname)
+            dldir = f'{folder}{file_name}'
+            r = requests.get(xurl, allow_redirects=True)
+            open(dldir, 'wb').write(r.content)
+            try:
+                file = filetype.guess(dldir)
+                xfiletype = file.mime
+            except AttributeError:
+                xfiletype = file_name
+            if xfiletype in ['video/mp4', 'video/x-matroska', 'video/webm', 'audio/mpeg']:
+                metadata = extractMetadata(createParser(dldir))
+                if metadata is not None:
+                    if metadata.has("duration"):
+                        duration = metadata.get('duration').seconds
+            await pablo.edit_text('Uploading...')
+            start_time = time.time()
+            if xfiletype in ['video/mp4', 'video/x-matroska', 'video/webm']:
+                video = await bot.send_video(
+                    chat_id=update.chat.id,
+                    video=dldir,
+                    caption=file_name,
+                    duration=duration,
+                    reply_to_message_id=update.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        pablo,
+                        start_time
+                    )
+                )
+                video_f = await video.forward(Config.LOG_CHANNEL)
+                await video_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>" + '\nLK21 URL: ' + url)
+            elif xfiletype == 'audio/mpeg':
+                audio = await bot.send_audio(
+                    chat_id=update.chat.id,
+                    audio=dldir,
+                    caption=file_name,
+                    duration=duration,
+                    reply_to_message_id=update.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        pablo,
+                        start_time
+                    )
+                )
+                audio_f = await audio.forward(Config.LOG_CHANNEL)
+                await audio_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>" + '\nLK21 URL: ' + url)
+            else:
+                doc = await bot.send_document(
+                    chat_id=update.chat.id,
+                    document=dldir,
+                    caption=file_name,
+                    reply_to_message_id=update.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        pablo,
+                        start_time
+                    )
+                )
+                doc_f = await doc.forward(Config.LOG_CHANNEL)
+                await doc_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>" + '\nLK21 URL: ' + url)
+            await pablo.delete()
+            shutil.rmtree(folder)
+            return
         if "|" in url:
             url_parts = url.split("|")
             if len(url_parts) == 2:
